80cfeb7e5b96bbce1720832f75db95dcb3c7c757
Libor Zak <libor.zak.zn@gmail.com>
2019-12-03 22:07:20 +0100

    Volání divočiny

--- src/Clock.js
+++ src/Clock.js
@@ -5,10 +5,18 @@ class Clock extends React.Component {
     hour: 12,
     minute: 0,
     day: "po",
-    isNigt: false
+    isNight: false,
+    days: ["pondělí", "úterý", "středa", "čtvrtek", "pátek", "sobota", "neděle"]
+  };
+
+  dayToNight = () => {
+    const dayOrNight = this.state.isNight;
+    this.setState({ isNight: !dayOrNight });
   };
 
   render() {
+    /*zajišťuje vykreslení komponenty na obrazovku 
+    a pokud v komponentě nastanou změny, tak se provádí opakovaně */
     console.log(this.state);
     let minuteState = "" + this.state.minute;
     if (this.state.minute < 10) {
@@ -19,8 +27,21 @@ class Clock extends React.Component {
         <p>
           {this.state.hour} : {minuteState} Den v týdnu: {this.state.day}{" "}
         </p>
-        <p>{this.state.isNigt ? "Je překrásná noc" : "Je nádherný den"}</p>
-        {/* if(this.state.isNigt) console.log ("je překrásná noc")*/}
+        <p>{this.state.isNight ? "Je překrásná noc" : "Je nádherný den"}</p>
+        {/* if(this.state.isNight) console.log ("je překrásná noc")*/}
+        <p>
+          {this.state.days[4]} velikost pole {this.state.days.length}
+        </p>
+        <button
+          onClick={() => {
+            console.log("klikl jsi mě, dobrý co?");
+            this.setState({ hour: 20 });
+          }}
+        >
+          klikni mě!
+        </button>
+        <br />
+        <button onClick={() => this.dayToNight()}>Nechť je den nocí</button>
       </div>
     );
   }
