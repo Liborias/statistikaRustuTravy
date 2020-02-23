7c6de7675a3070648c6f2fa3bde8acb38025f00b
Jirina Brezinova <jirina.brezinova@evalue.cz>
2020-02-23 18:39:35 +0100

    funkcionální komponenta - bude konflikt!! :D

--- src/Components/RenderTask.js
+++ src/Components/RenderTask.js
@@ -1,37 +1,40 @@
 import React from "react";
 
-class renderTask extends React.Component {
-  
-    const { tasks } = this.state;
-    const vysledneDivy = tasks.map(x => (
-      <div
-        className={`task priority${x.priority} ${x.isDone ? "done" : ""}`}
-        key={x.id}
-      >
-        <div className="taskRow">
-          <span>
-            {x.priority} {x.taskName}
-          </span>
-          <span>
-            <button
-              className="delButton"
-              onClick={() => this.setState({ showModalData: x })}
-            >
-              Detail
+function RenderTask(props) {
+
+  const { tasks } = props;
+  const vysledneDivy = tasks.map(x => (
+    <div
+      className={`task priority${x.priority} ${x.isDone ? "done" : ""}`}
+      key={x.id}
+    >
+      <div className="taskRow">
+        <span>
+          {x.priority} {x.taskName}
+        </span>
+        <span>
+          <button
+            className="delButton"
+            onClick={() => this.setState({ showModalData: x })}
+          >
+            Detail
+          </button>
+          {!x.isDone && (
+            <button className="donButton" onClick={() => this.setDone(x.id)}>
+              Done
             </button>
-            {!x.isDone && (
-              <button className="donButton" onClick={() => this.setDone(x.id)}>
-                Done
-              </button>
-            )}
-            <button className="delButton" onClick={() => this.remove(x.id)}>
-              Delete
-            </button>
-          </span>
-        </div>
+          )}
+          <button className="delButton" onClick={() => this.remove(x.id)}>
+            Delete
+          </button>
+        </span>
       </div>
-    ));
-    return vysledneDivy;
-  };
+    </div>
+  ));
+
+   return(
+      <div>vysledneDivy</div>
+   );
+
 }
 export default renderTask;
